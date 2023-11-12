e29fecb8119ba63d7306747453483fb18aec326d
SaraR-1 <s.romiti01@gmail.com>
2023-11-12 17:15:45 +0000

    added footprint

--- aerialimagesdetection/inference/detection.py
+++ aerialimagesdetection/inference/detection.py
@@ -1,26 +1,40 @@
 from ultralytics import YOLO
+import pandas as pd
 from pathlib import Path
 from PIL import Image
+import wandb
+from wandb.integration.ultralytics import add_wandb_callback
 
+# Initialize W&B run
+wandb.init(project="ultralytics", job_type="inference")
 output = (Path(__file__).parents[2] / "data" / "output")
 output.mkdir(parents=True, exist_ok=True)
 
-(output / "objects.csv").touch()
-
 (output/ "annotated_images").mkdir(parents=True, exist_ok=True)
 
-
 model_path = Path(__file__).parents[2] / "runs" / "detect" / "train2" / "weights" / "best.pt"
-# Load a model
-model = YOLO(model_path)  # load a pretrained model (recommended for training)
-
 # Define path to directory containing images and videos for inference
 source = Path(__file__).parents[2] / "data" / "upscaled_data"
+
+objects_pd = pd.DataFrame(columns=["image_name", "class", "x1", "y1", "x2", "y2"])
+# Load a model
+model = YOLO(model_path)  # load a pretrained model (recommended for training)
+# Add W&B callback for Ultralytics
+add_wandb_callback(model, enable_model_checkpointing=True)
 # Run inference on the source - save results to the output directory 
 results = model(source, stream=True)
 
 # Show the results
 for e, r in enumerate(results):
+    boxes = r.boxes
+    classes, boxes_coord = boxes.cls, boxes.xyxy
+    image_name = Path(r.path).name
+    objects_pd = objects_pd.append({"image_name": image_name, "class": classes, "x1": boxes_coord[0], "y1": boxes_coord[1], "x2": boxes_coord[2], "y2": boxes_coord[3]}, ignore_index=True)
     im_array = r.plot()  # plot a BGR numpy array of predictions
     im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
-    im.save(str(output / 'annotated_images' / f'results_{e}.jpg'))  # save image
+    im.save(str(output / 'annotated_images' / image_name))  # save image
+    
+objects_pd.to_csv(output / "objects.csv", index=False)
+
+# Finish the W&B run
+wandb.finish()
--- aerialimagesdetection/inference/footprint.py
+++ aerialimagesdetection/inference/footprint.py
@@ -1,8 +1,30 @@
 from pathlib import Path
+from ultralytics import SAM
 
-output = (Path(__file__).parents[2] / "output")
+output = (Path(__file__).parents[2] / "data" / "output")
 output.mkdir(parents=True, exist_ok=True)
 
 (output / "footprints.csv").touch()
 
 (output/ "masked_images").mkdir(parents=True, exist_ok=True)
+
+
+
+# Load a model
+model = SAM('sam_b.pt')
+
+# Display model information (optional)
+model.info()
+
+source = Path(__file__).parents[2] / "data" / "upscaled_data"
+
+# # Run inference with bboxes prompt
+# model(str(source / "image_1.jpeg"), bboxes=[954.1512,  243.0841, 1090.0707,  320.6259], save_dir=output / "masked_images")
+# breakpoint()
+
+
+from ultralytics.data.annotator import auto_annotate
+det_model = Path(__file__).parents[2] / "runs" / "detect" / "train2" / "weights" / "best.pt"
+auto_annotate(data=source, det_model=det_model, sam_model='sam_b.pt', output_dir=output / "masked_images")
+
+breakpoint()
