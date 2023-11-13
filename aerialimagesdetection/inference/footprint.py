9277647c127f3df51b7c477a6bb224683a704710
SaraR-1 <s.romiti01@gmail.com>
2023-11-13 11:54:02 +0000

    initial super resolution

--- aerialimagesdetection/inference/footprint.py
+++ aerialimagesdetection/inference/footprint.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from ultralytics import SAM
+from ultralytics.utils.plotting import Annotator
 
 output = (Path(__file__).parents[2] / "data" / "output")
 output.mkdir(parents=True, exist_ok=True)
@@ -18,13 +19,10 @@ model.info()
 
 source = Path(__file__).parents[2] / "data" / "upscaled_data"
 
-# # Run inference with bboxes prompt
-# model(str(source / "image_1.jpeg"), bboxes=[954.1512,  243.0841, 1090.0707,  320.6259], save_dir=output / "masked_images")
-# breakpoint()
-
-
 from ultralytics.data.annotator import auto_annotate
 det_model = Path(__file__).parents[2] / "runs" / "detect" / "train2" / "weights" / "best.pt"
 auto_annotate(data=source, det_model=det_model, sam_model='sam_b.pt', output_dir=output / "masked_images")
+
+Annotator(im = str(source / "image_1.jpeg")
 
 breakpoint()
--- aerialimagesdetection/inference/super_resolution.py
+++ aerialimagesdetection/inference/super_resolution.py
@@ -1,8 +1,22 @@
-import argparse
-import shutil
+import cv2
+from pathlib import Path
 
-def passthrough():
-    shutil.copytree("data", "upscaled_data")
+    
+input = (Path(__file__).parents[2] / "data" / "raw_data")
+output = (Path(__file__).parents[2] / "data" / "upscaled_data")
+output.mkdir(parents=True, exist_ok=True)
 
-if __name__ == "__main__":
-    passthrough()
+model_path = (Path(__file__).parents[2] / "EDSR_x3.pb")
+
+images = [f for f in Path(input).glob( "*.jpeg")]
+ 
+sr = cv2.dnn_superres.DnnSuperResImpl_create()
+sr.readModel(str(model_path))
+sr.setModel("edsr", 3) # set the model by passing the value and the upsampling ratio
+
+for img_name in images:
+    # Read image
+    image = cv2.imread(str(img_name))
+    result = sr.upsample(image) # upscale the input image
+    # Save the image
+    cv2.imwrite(str(output / img_name.name), result)
